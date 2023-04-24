dceef275f1bbc11fe3e5de2a86e066c3e4c954ad
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-25 00:55:38 +0100

    Compile all .ts files to .js files

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,26 +1,26 @@
 import {
-    Request,
-    Response,
-    NextFunction,
-    ErrorRequestHandler,
-  } from "express";
-  import createHttpError from "http-errors";
-  
-  export const catchErrorAndForward = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => next(createHttpError(404));
-  
-  const errorHandler = ((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error =
-      req.app.get("env") === "development" ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-  }) as ErrorRequestHandler;
-  
-  export default errorHandler;
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
+import createHttpError from "http-errors";
+
+export const catchErrorAndForward = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => next(createHttpError(404));
+
+const errorHandler = ((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error =
+    req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+}) as ErrorRequestHandler;
+
+export default errorHandler;
