4dc8aa98b2db7c52090fd92ddb9c6200337ed94b
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-25 01:42:06 +0100

    Add more fields to UserSchema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,10 +1,25 @@
-import { Schema, model } from "mongoose";
+import { Document, Schema, model } from "mongoose";
+
+interface IUserSchema {
+  email: string;
+  username: string;
+  password: string;
+  last_name: string;
+  first_name: string;
+  phone_numner?: string;
+}
+
+interface IUser extends Document, IUserSchema {}
 
 const UserSchema = new Schema({
+  phone_numner: String,
+  email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
+  last_name: { type: String, required: true },
+  first_name: { type: String, required: true },
 });
 
-const User = model("User", UserSchema);
+const User = model<IUser>("User", UserSchema);
 
 export default User;
