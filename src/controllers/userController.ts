e64ba11c3348268c538684cfdcc0b9afc62776f6
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-25 12:53:18 +0100

    Set user account to display only pending and completed transactions

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,11 +9,7 @@ const user_get = async (
   if (!res.locals.currentUser._id) res.redirect("/");
   const userId = res.locals.currentUser._id;
 
-  const [approved, pending, completed] = await Promise.all([
-    Transaction.find({
-      user: userId,
-      status: "approved",
-    }),
+  const [pending, completed] = await Promise.all([
     Transaction.find({
       user: userId,
       status: "pending",
@@ -21,11 +17,11 @@ const user_get = async (
     Transaction.find({
       user: userId,
       status: ["cancelled", "approved"],
-    }).limit(5),
+    }),
   ]);
-  console.log(pending, approved, completed);
+  console.log(pending, completed);
 
-  res.render("user", { approved, pending, completed });
+  res.render("user", { pending, completed });
 };
 
 export default user_get;
