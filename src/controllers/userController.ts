b864e2f92c3268ed09d5816a4389c9e927ad14aa
Letam Bossman Barinua <letambarinua@gmail.com>
2023-05-09 14:59:01 +0100

    Add form validation to buy form

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import Transaction from "../models/transactionModel";
 import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
 
 // Renders user page
 const user_get = asyncHandler(
@@ -40,8 +41,28 @@ export const user_buy_get = (req: Request, res: Response) => {
 };
 
 // Sends buy form details to db and render user page
-export const user_buy_post = asyncHandler(
-  async (req: Request, res: Response) => {
+export const user_buy_post = [
+  body("crypto")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Crypto Symbol must have at least 3 characters")
+    .escape(),
+
+  body("amount")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Amount must have a number")
+    .isNumeric()
+    .withMessage("Amount must have a number")
+    .escape(),
+
+  body("walletAddress")
+    .trim()
+    .isBtcAddress()
+    .withMessage("Wallet Address Should be a BTC wallet address")
+    .escape(),
+
+  asyncHandler(async (req: Request, res: Response) => {
     const id = req.params.id;
     const { amount, walletAddress, crypto } = req.body;
     const transaction = new Transaction({
@@ -51,10 +72,31 @@ export const user_buy_post = asyncHandler(
       walletAddress,
       paymentScreenshot: req.file?.filename,
     });
-    await transaction.save();
-    res.redirect(`/user/${id}`);
-  }
-);
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = errors.array();
+
+      // Options object to pass to view template.
+      const options = {
+        id,
+        error,
+        transaction,
+        style: "login",
+      };
+
+      /** There are errors. Render the form again *
+       ** with sanitized values/error messages ****/
+      res.render("buy", options);
+
+      return;
+    } else {
+      await transaction.save();
+      res.redirect(`/user/${id}`);
+    }
+  }),
+];
 
 // Renders a single transaction page
 export const user_details_get = asyncHandler(
